f09f14d0236a549342c5a22924a44ab9b2db5449
davidmartinez13 <dsmartinez13@hotmail.com>
2024-02-05 17:58:04 +0000

    feat: added look to point python version

--- src/a_finial_project_robcup_athome/scripts/follow_point.py
+++ src/a_finial_project_robcup_athome/scripts/follow_point.py
@@ -1,75 +1,64 @@
 #!/usr/bin/env python
-
 import rospy
-import actionlib
 import cv2
-from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import PointStamped
 from control_msgs.msg import PointHeadAction, PointHeadGoal
+import actionlib
 import numpy as np
-
 
 class LookToPoint:
     def __init__(self):
-        self.node_name = "look_to_point"
-        rospy.init_node(self.node_name)
+        # Initialize ROS node
+        rospy.init_node("look_to_point")
+        rospy.loginfo("Starting look_to_point application ...")
 
-        self.window_name = "Inside of TIAGo's head"
+        # Global variables
+        self.window_name = "head"
         self.camera_frame = "/xtion_rgb_optical_frame"
         self.image_topic = "/xtion/rgb/image_raw"
         self.camera_info_topic = "/xtion/rgb/camera_info"
-
         self.camera_intrinsics = None
         self.latest_image_stamp = None
-        self.bridge = CvBridge()
+        self.point_head_client = None
 
-        self.point_head_client = actionlib.SimpleActionClient('/head_controller/point_head_action', PointHeadAction)
-        rospy.loginfo("Waiting for head controller action server...")
-        # self.point_head_client.wait_for_server()
+        # Create a point head action client
+        self.create_point_head_client()
 
-        rospy.Subscriber(self.camera_info_topic, CameraInfo, self.camera_info_callback)
-        rospy.Subscriber(self.image_topic, Image, self.image_callback, queue_size=1)
+        # Subscribe to the image topic
+        rospy.loginfo("Subscribing to " + self.image_topic + " ...")
+        rospy.Subscriber(self.image_topic, Image, self.image_callback)
 
-        cv2.namedWindow(self.window_name)
+    # ROS callback for every new image received
+    def image_callback(self, img_msg):
+        self.latest_image_stamp = img_msg.header.stamp
+
+        cv_img = CvBridge().imgmsg_to_cv2(img_msg, "bgr8")
+        cv2.imshow(self.window_name, cv_img)
         cv2.setMouseCallback(self.window_name, self.on_mouse)
+        cv2.waitKey(1)
 
-    def camera_info_callback(self, msg):
-        self.camera_intrinsics = np.zeros((3, 3), dtype=np.float64)
-        self.camera_intrinsics[0, 0] = msg.K[0] # fx
-        self.camera_intrinsics[1, 1] = msg.K[4] # fy
-        self.camera_intrinsics[0, 2] = msg.K[2] # cx
-        self.camera_intrinsics[1, 2] = msg.K[5] # cy
-        self.camera_intrinsics[2, 2] = 1
-
-
-    def image_callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
-
-        cv2.imshow(self.window_name, cv_image)
-        cv2.waitKey(3)
-        self.latest_image_stamp = data.header.stamp
-
+    # OpenCV callback function for mouse events on a window
     def on_mouse(self, event, u, v, flags, param):
         if event != cv2.EVENT_LBUTTONDOWN:
             return
 
-        rospy.loginfo("Pixel selected ({}, {}) Making TIAGo look to that direction".format(u, v))
+        rospy.loginfo(f"Pixel selected ({u}, {v}) Making TIAGo look to that direction")
 
         point_stamped = PointStamped()
         point_stamped.header.frame_id = self.camera_frame
         point_stamped.header.stamp = self.latest_image_stamp
 
+        # Compute normalized coordinates of the selected pixel
         x = (u - self.camera_intrinsics[0, 2]) / self.camera_intrinsics[0, 0]
         y = (v - self.camera_intrinsics[1, 2]) / self.camera_intrinsics[1, 1]
-        Z = 1.0 # Define an arbitrary distance
+        Z = 1.0  # Define an arbitrary distance
         point_stamped.point.x = x * Z
         point_stamped.point.y = y * Z
         point_stamped.point.z = Z
 
+        # Build the action goal
         goal = PointHeadGoal()
         goal.pointing_frame = self.camera_frame
         goal.pointing_axis.x = 0.0
@@ -82,10 +71,32 @@ class LookToPoint:
         self.point_head_client.send_goal(goal)
         rospy.sleep(0.5)
 
-if __name__ == '__main__':
-    try:
-        look_to_point = LookToPoint()
+    # Create a ROS action client to move TIAGo's head
+    def create_point_head_client(self):
+        rospy.loginfo("Creating action client to head controller ...")
+        self.point_head_client = actionlib.SimpleActionClient("/head_controller/point_head_action", PointHeadAction)
+        self.point_head_client.wait_for_server(rospy.Duration(25))
+
+    # Run the node
+    def run(self):
+        # Get the camera intrinsic parameters from the appropriate ROS topic
+        rospy.loginfo("Waiting for camera intrinsics ... ")
+        msg = rospy.wait_for_message(self.camera_info_topic, CameraInfo)
+        if msg is not None:
+            fx = msg.K[0]
+            fy = msg.K[4]
+            cx = msg.K[2]
+            cy = msg.K[5]
+            self.camera_intrinsics = np.array([[fx, 0, cx],
+                                               [0, fy, cy],
+                                               [0, 0, 1]])
+
+        # Enter a loop that processes ROS callbacks. Press CTRL+C to exit the loop
         rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
-    cv2.destroyAllWindows()
+
+        # Destroy the OpenCV window
+        cv2.destroyWindow(self.window_name)
+
+if __name__ == '__main__':
+    look_to_point = LookToPoint()
+    look_to_point.run()
