1fd173867bdab78251e786dbe40a0aee2305ea9f
Margaux Edwards <n10212884@qut.edu.au>
2024-02-13 20:38:27 +0000

    final nav code

--- src/tiago_robocup_navigation/scripts/create_bag_goal.py
+++ src/tiago_robocup_navigation/scripts/create_bag_goal.py
@@ -6,20 +6,21 @@ from geometry_msgs.msg import PoseStamped, PointStamped
 import math
 
 # Topic names and frame ids as constants for easy modification
-NODE_NAME = "get_target_point" # Name of the ROS node
-GOAL_SUBSCRIBE = "/goal_point" # Topic to subscribe to for the goal point from the camera
-GOAL_PUBLISH = "/human_goal" # Topic to publish the goal point to move_base
+NODE_NAME = "create_bag_goal" # Name of the ROS node
+GOAL_SUBSCRIBE = "/pick_point" # Topic to subscribe to for the goal point from the camera
+GOAL_PUBLISH = "/bag_goal" # Topic to publish the goal point to move_base
 CAMERA_FRAME_ID = "xtion_rgb_optical_frame" # Frame id of the camera
-MAP_FRAME_ID = "map" # Frame id of the map
+MAP_FRAME_ID = "map" 
 
 class GetTargetPoint:
     def __init__(self):
         # Initialize ROS nodes
-        # rospy.init_node(NODE_NAME, anonymous=True)
+        rospy.init_node(NODE_NAME, anonymous=True)
         rospy.Subscriber(GOAL_SUBSCRIBE, PointStamped, self.point_callback)
         self.listener = tf.TransformListener()
         self.goal_pub = rospy.Publisher(GOAL_PUBLISH, PoseStamped, queue_size=2)
-        rospy.loginfo("GetTargetPoint node initialized")
+        rospy.loginfo("f{NODE_NAME} initialized")
+        
 
     def point_callback(self, msg):
         camera_point = geometry_msgs.msg.PointStamped()
@@ -27,7 +28,6 @@ class GetTargetPoint:
         camera_point.point = msg.point
 
         if camera_point.point.x is None or camera_point.point.y is None or camera_point.point.z is None:
-            
             rospy.loginfo("No point received from the camera")
             return
 
@@ -41,9 +41,9 @@ class GetTargetPoint:
             goal_point = geometry_msgs.msg.PoseStamped()
             goal_point.header.frame_id = MAP_FRAME_ID
             goal_point.header.stamp = rospy.Time.now()
-            goal_point.pose.position.x = transformed_point.point.x
-            goal_point.pose.position.y = transformed_point.point.y - 0.5 * math.sin(math.atan2(transformed_point.point.y, transformed_point.point.x))
-            goal_point.pose.position.z = transformed_point.point.z
+            goal_point.pose.position.x = transformed_point.point.x - (0.3 + 0.3) * math.cos(math.atan2(transformed_point.point.y, transformed_point.point.x))
+            goal_point.pose.position.y = transformed_point.point.y + (0.3 + 0.3) * math.sin(math.atan2(transformed_point.point.y, transformed_point.point.x))
+            goal_point.pose.position.z = 0.0
             
             # Calculate orientation
             quaternion = tf.transformations.quaternion_from_euler(0, 0, math.atan2(transformed_point.point.y, transformed_point.point.x))
@@ -52,7 +52,7 @@ class GetTargetPoint:
             goal_point.pose.orientation.z = quaternion[2]
             goal_point.pose.orientation.w = quaternion[3]
             
-            rospy.loginfo("Publishing Goal point: x=%f, y=%f, z=%f" % (goal_point.pose.position.x, goal_point.pose.position.y, goal_point.pose.position.z))
+            # rospy.loginfo("Publishing Goal point: x=%f, y=%f, z=%f" % (goal_point.pose.position.x, goal_point.pose.position.y, goal_point.pose.position.z))
             self.goal_pub.publish(goal_point)
 
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
